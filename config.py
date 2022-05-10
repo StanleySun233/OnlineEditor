8f101a4035565d86a76872882cb9683260ddafda
Stanley Sun <staleysun233@163.com>
2022-05-10 21:27:09 +0800

    添加：添加全局变量设置。

---
+++ config.py
@@ -0,0 +1,11 @@
+httpLink = '127.0.0.1'
+httpPort = '5000'
+httpUrl = 'http://{}:{}'.format(httpLink, httpPort)
+
+sqlUrl = 'http://localhost:3306'
+sqlAccount = 'root'
+sqlPassword = 'root'
+
+minioUrl = 'http://47.100.93.63:9090'
+minioAccount = 'minioadmin'
+minioPassword = 'admin123456'
