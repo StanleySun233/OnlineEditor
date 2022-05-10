c4f3edfa5b675c219865b4acc298f52c20b1cb18
Stanley Sun <staleysun233@163.com>
2022-05-10 21:27:01 +0800

    添加：添加后端api接口实现。

--- backend/backend.py
+++ backend/backend.py
@@ -4,7 +4,7 @@ import fun
 import sql
 
 
-def login(sqlClient: sql.sqlClient, attrs: dict):
+def userAuth(sqlClient: sql.sqlClient, attrs: dict):
     res = {'code': 0, 'data': None, 'msg': None}
     if 'account' not in attrs.keys():
         res['msg'] = '请求参数没有账号'
@@ -16,7 +16,7 @@ def login(sqlClient: sql.sqlClient, attrs: dict):
     password = attrs['password']
     data = sqlClient.isExist('user_info', {'user_account': account, 'user_password': password})
     if data:
-        auth = sqlClient.searchInfo('user_info', {'user_account': account})[0][2]
+        auth = sqlClient.searchInfo('user_info', {'user_account': account}, val=['user_auth'])[0]
         res['data'] = {'auth': int(auth)}
         res['msg'] = '登录成功'
         res['code'] = 1
@@ -25,7 +25,7 @@ def login(sqlClient: sql.sqlClient, attrs: dict):
     return json.dumps(res, ensure_ascii=False)
 
 
-def register(sqlClient: sql.sqlClient, attrs: dict):
+def userRegister(sqlClient: sql.sqlClient, attrs: dict):
     res = {'code': 0, 'data': None, 'msg': None}
 
     if 'account' not in attrs.keys():
@@ -50,3 +50,16 @@ def register(sqlClient: sql.sqlClient, attrs: dict):
         res['code'] = 1
         res['data'] = {'auth': 0}
     return json.dumps(res, ensure_ascii=False)
+
+
+def userInfoGetInfoByAccount(sqlClient: sql.sqlClient, attrs: dict):
+    res = {'code': 0, 'data': None, 'msg': None}
+    data = sqlClient.isExist('user_info', {'user_account': attrs['account']})
+    if data:
+        sheet = sqlClient.searchInfo('user_info', {'user_account': attrs['account']})
+        res['data'] = [i for i in sheet]
+        res['code'] = 1
+        res['msg'] = '成功'
+    else:
+        res['msg'] = '找不到该用户'
+    return res
