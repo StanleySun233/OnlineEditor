6ce69d47ac828df1ea2dca991ef43cf1b3cb947a
Stanley Sun <staleysun233@163.com>
2022-05-11 23:34:32 +0800

    更新：测试版本可以同时打开前后端。

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import threading
+
+import backend.controller
 import frontend
+
+app = threading.Thread(target=backend.controller.run)
+app.start()
 
 loginWindow = frontend.loginWindow.LoginWindow(400, 300, '登录界面')
